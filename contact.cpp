2c2ac1df5d369dcb2bd8f6f49c6f9bf5aa6c1925
Donya82 <123035772+Donya82@users.noreply.github.com>
2024-04-02 23:47:10 -0700

    Update contact.cpp

--- contact.cpp
+++ contact.cpp
@@ -5,22 +5,19 @@
 #include <algorithm> // For std::remove
 using namespace std;
 
-Email::Email(string type, string email_addr){
-    this->type = type;
-    this->email_addr = email_addr;
-}
-
-<<<<<<< HEAD
-=======
+Email::Email(string type, string email_addr) : type(type), email_addr(email_addr) {}
 
 
->>>>>>> ab637fd03fb95644dbfe32f448fe46bdf9637048
+
+
 void Email::set_contact(){
     cout << "Enter the type of email address: ";
     cin >> type;
     cout << "Enter email address: ";
     cin >> email_addr;
 }
+
+
 
 string Email::get_contact(string style){
     
@@ -30,29 +27,15 @@ string Email::get_contact(string style){
         return email_addr;
 }
 
+
 void Email::print(){
     
 	cout << get_contact() << endl;
 }
 
-Phone::Phone(string type, string num){
 
-<<<<<<< HEAD
-=======
-Phone::Phone(string type, string num){
-
->>>>>>> ab637fd03fb95644dbfe32f448fe46bdf9637048
-    this->type = type;
-
-    for (char &c : num) {
-        if (c == '-') {
-            num.erase(std::remove(num.begin(), num.end(), '-'), num.end());
-            break;
-        }
-    }
+Phone::Phone(string type, string num) : type(type) {
+    // Remove '-' from num
+    num.erase(remove(num.begin(), num.end(), '-'), num.end());
     this->num = num;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ab637fd03fb95644dbfe32f448fe46bdf9637048
