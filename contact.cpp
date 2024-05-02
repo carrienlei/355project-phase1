f8816526523c339db7a54d5f313e134f54427312
ehuber1 <ehuber@usc.edu>
2024-05-01 21:14:48 -0700

    Update contact.cpp

--- contact.cpp
+++ contact.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "contact.h"
-// TODO: Add needed libraries! <- In contact.h
 
 Email::Email(){
     set_contact();
@@ -13,8 +12,6 @@ Email::Email(string type, string email_addr){
 }
 
 void Email::set_contact(){
-    // TODO: Do not change the prompts!
-
     cout << "Enter the type of email address: ";
     // some code here
     std::getline(std::cin, type);
@@ -24,7 +21,6 @@ void Email::set_contact(){
 }
 
 string Email::get_contact(string style){
-    // Note: We have default argument in declaration and not in definition!
     if (style=="full")
 	    return "Email (" + type + "): " + email_addr;
     else
@@ -32,7 +28,6 @@ string Email::get_contact(string style){
 }
 
 void Email::print(){
-    // Note: get_contact is called with default argument
 	cout << get_contact("full") << endl;
 }
 
@@ -41,11 +36,6 @@ Phone::Phone(){
 }
 
 Phone::Phone(string type, string num){
-    // TODO: It is possible that num includes "-" or not, manage it!
-    // TODO: Complete this method!
-    // Note: We don't want to use C++11! stol is not valid!
-    // The code has been covered in the discussion session
-
     if (num.length() < 10){
         cout << "Error! Phone number won't be saved. Please enter a 10-digit phone number" << endl;
         return;
@@ -59,10 +49,7 @@ Phone::Phone(string type, string num){
     this->phone_num = num;
 }
 
-//Prompts the user to enter the information, like phone number or email address
 void Phone::set_contact(){
-    // TODO: Complete this method
-    // Use the same prompts as given!
 	cout <<"Enter the type of phone number: ";
 	std::getline(std::cin, type);
 	string num;
@@ -76,23 +63,12 @@ void Phone::set_contact(){
 }
 
 string Phone::get_contact(string style){
-    // TODO: Complete this method, get hint from Email
-    // The code has been covered in discussion session
-
 	if (style=="full")
 		return "Phone (" + type + "): " + phone_num;
     else
         return phone_num;
-
-    /* Note: Understand your own TA's bug!
-     * This code has a bug, can you understand it!
-    int x1 = (phone_num/10000000);
-	int x2 = ((phone_num)/10000)%1000;
-	int x3 = phone_num%10000;
-    */
 }
 
 void Phone::print(){
-    // Note: get_contact is called with default argument << changed
 	cout << get_contact("full") << endl;
 }
